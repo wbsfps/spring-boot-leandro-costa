0ea3572586d551dea09eb7d284f49ce74007f191
William Santana <williamandrade1058@gmail.com>
2024-09-06 14:35:53 -0300

    retornando para o dozer

--- src/main/java/br/com/erudio9/mapper/DozerMapper.java
+++ src/main/java/br/com/erudio9/mapper/DozerMapper.java
@@ -4,12 +4,15 @@ package br.com.erudio9.mapper;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 
+// import org.modelmapper.ModelMapper;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class DozerMapper {
 
     private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+    // private static ModelMapper mapper = new ModelMapper();
 
     public static <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
